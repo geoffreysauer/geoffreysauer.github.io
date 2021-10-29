0a62e5120e2526c5b49449367e46f453dae3fac7
Geoffrey Sauer <gsauer@iastate.edu>
2021-10-29 10:45:50 -0500

    Added DITA WebHelp
    Added DITA WebHelp

---
+++ dita/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,28 @@
+define(function() {var keywords=[{w:"My",p:["p0","p1"]},{w:"Home",p:["p0"]},{w:"Life",p:["p0","p1"]},{w:"Work",p:["p1","p18","p20"]},{w:"Charlie",p:["p2"]},{w:"Kostelnick",p:["p2"]},{w:"Reference",p:["p3","p4","p16"]},{w:"2",p:["p3","p8"]},{w:"3",p:["p4","p9"]},{w:"University",p:["p5"]},{w:"of",p:["p5"]},{w:"Notre",p:["p5"]},{w:"Dame,",p:["p5"]},{w:"South",p:["p5"]},{w:"Bend,",p:["p5"]},{w:"IN",p:["p5"]},{w:"Carnegie",p:["p6"]},{w:"Mellon",p:["p6"]},{w:"University,",p:["p6","p12"]},{w:"Pittsburgh,",p:["p6"]},{w:"PA",p:["p6"]},{w:"Web",p:["p7"]},{w:"Design",p:["p7"]},{w:"Skill",p:["p8","p9","p10"]},{w:"4",p:["p10"]},{w:"The",p:["p11"]},{w:"Copy",p:["p11"]},{w:"Stop,",p:["p11"]},{w:"Mobile,",p:["p11"]},{w:"AL",p:["p11"]},{w:"Iowa",p:["p12"]},{w:"State",p:["p12"]},{w:"Ames",p:["p12"]},{w:"IA",p:["p12"]},{w:"about",p:["p13","p14"]},{w:"Me",p:["p14"]},{w:"education",p:["p15"]},{w:"References",p:["p17"]},{w:"Skills",p:["p19"]},{w:"Experience",p:["p20"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[0, 3, 2];
+ph["p2"]=[4, 5];
+ph["p3"]=[6, 7];
+ph["p4"]=[6, 8];
+ph["p5"]=[9, 10, 11, 12, 13, 14, 15];
+ph["p6"]=[16, 17, 18, 19, 20];
+ph["p7"]=[21, 22];
+ph["p8"]=[23, 7];
+ph["p9"]=[23, 8];
+ph["p10"]=[23, 24];
+ph["p20"]=[3, 39];
+ph["p12"]=[30, 31, 18, 32, 33];
+ph["p11"]=[25, 26, 27, 28, 29];
+ph["p14"]=[34, 35];
+ph["p13"]=[34];
+ph["p16"]=[6];
+ph["p15"]=[36];
+ph["p18"]=[3];
+ph["p17"]=[37];
+ph["p19"]=[38];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ dita/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {11:17,13:-1,9:16,14:-1,4:15,0:13,16:-1,2:15,1:13,5:14,3:15,15:-1,17:-1,8:16,10:16,6:14,12:17,7:16};
+});
